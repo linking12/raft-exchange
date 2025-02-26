f2c8d364ceeeb4275ec6dc7441aa68890d1e3b86
ming <ming@binance.com>
2025-02-26 11:32:00 +0800

    add raft

--- raft-exchange-server/src/main/java/com/binance/raftexchange/server/RaftexchangeApplication.java
+++ raft-exchange-server/src/main/java/com/binance/raftexchange/server/RaftexchangeApplication.java
@@ -2,6 +2,7 @@ package com.binance.raftexchange.server;
 
 import java.io.IOException;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,16 +12,23 @@ import io.grpc.Server;
 import io.grpc.ServerBuilder;
 
 @SpringBootApplication
-public class RaftexchangeApplication {
+public class RaftexchangeApplication implements CommandLineRunner {
     public static void main(String[] args) throws Exception {
-        Server server = ServerBuilder.forPort(5001).addService(new UserServiceImpl()).build();
         SpringApplication.run(RaftexchangeApplication.class, args);
+        // 这里先把Jgroup的raft起起来
+        // 然后再起暴露GrpcServer
+    }
+
+    @Override
+    public void run(String... arg0) throws Exception {
+        Server server = ServerBuilder.forPort(5001).addService(new UserServiceImpl()).build();
         try {
             server.start();
             server.awaitTermination();
         } catch (InterruptedException | IOException e) {
             throw new Exception(e.getMessage());
         }
+
     }
 
 }
