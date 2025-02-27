e5c451589bf7928f2be1af91de1129fe3698f08c
ming <ming@binance.com>
2025-02-27 22:26:01 +0800

    update

--- raft-exchange-server/src/main/java/com/binance/raftexchange/server/grpc/GrpcServerContainer.java
+++ raft-exchange-server/src/main/java/com/binance/raftexchange/server/grpc/GrpcServerContainer.java
@@ -10,27 +10,27 @@ import io.grpc.ServerBuilder;
 
 public class GrpcServerContainer {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(GrpcServerContainer.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(GrpcServerContainer.class);
 
-	private final RaftClusterContainer raftClusterContainer;
+    private final RaftClusterContainer raftClusterContainer;
 
-	private Server server;
+    private Server server;
 
-	public GrpcServerContainer(RaftClusterContainer raftClusterContainer) {
-		this.raftClusterContainer = raftClusterContainer;
-	}
+    public GrpcServerContainer(RaftClusterContainer raftClusterContainer) {
+        this.raftClusterContainer = raftClusterContainer;
+    }
 
-	public void doStart() throws Exception {
-		String grpcPort = System.getProperty("grpc.port", "5001");
-		this.server = ServerBuilder.forPort(Integer.valueOf(grpcPort))//
-				.addService(new ApiService(raftClusterContainer))//
-				.addService(new OrderService(raftClusterContainer))//
-				.build();
-		Server localServer = server.start();
-		LOGGER.info("grpc server start {}", localServer.getPort());
-	}
+    public void doStart() throws Exception {
+        String grpcPort = System.getProperty("grpc.port", "5001");
+        this.server = ServerBuilder.forPort(Integer.valueOf(grpcPort))//
+            .addService(new ApiService(raftClusterContainer))//
+            .addService(new OrderService(raftClusterContainer))//
+            .build();
+        Server localServer = server.start();
+        LOGGER.info("grpc server start {}", localServer.getPort());
+    }
 
-	public void doStop() throws Exception {
-		server.shutdown();
-	}
+    public void doStop() throws Exception {
+        server.shutdown();
+    }
 }
