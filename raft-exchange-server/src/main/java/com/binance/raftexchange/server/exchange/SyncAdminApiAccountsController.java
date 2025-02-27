b07fa16afeab38f269697ecf7a175911751fc04a
bob.dong <bob.dong@binance.com>
2025-02-27 17:33:21 +0400

    update

--- raft-exchange-server/src/main/java/com/binance/raftexchange/server/exchange/SyncAdminApiAccountsController.java
+++ raft-exchange-server/src/main/java/com/binance/raftexchange/server/exchange/SyncAdminApiAccountsController.java
@@ -1,8 +1,37 @@
 package com.binance.raftexchange.server.exchange;
 
+import com.binance.raftexchange.stubs.api.ApiAddUser;
+import com.binance.raftexchange.stubs.api.ApiAdjustUserBalance;
+import exchange.core2.core.ExchangeApi;
+import exchange.core2.core.common.cmd.CommandResultCode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.CompletableFuture;
+
 public class SyncAdminApiAccountsController {
 
-	
-	
-	
+    private static final Logger LOG = LoggerFactory.getLogger(SyncAdminApiAccountsController.class);
+
+    public static void createUser(ApiAddUser grpcApiAddUser) throws Exception {
+        exchange.core2.core.common.api.ApiAddUser apiAddUser =
+                exchange.core2.core.common.api.ApiAddUser.builder().uid(grpcApiAddUser.getUid()).build();
+        LOG.info("ApiAddUser applied, msg: {}", apiAddUser);
+
+        ExchangeApi api = ExchangeApiInstance.exchangeApi();
+        CompletableFuture<CommandResultCode> future = api.submitCommandAsync(apiAddUser);
+        CommandResultCode resultCode = future.get();
+        LOG.info("ApiAddUser called, result: {}", resultCode);
+    }
+
+    public static void adjustBalance(ApiAdjustUserBalance grpcApiAdjustUserBalance) throws Exception {
+        exchange.core2.core.common.api.ApiAdjustUserBalance apiAdjustUserBalance = exchange.core2.core.common.api.ApiAdjustUserBalance.builder()
+                .uid(grpcApiAdjustUserBalance.getUid())
+                .currency(grpcApiAdjustUserBalance.getCurrency())
+                .amount(grpcApiAdjustUserBalance.getAmount())
+                .transactionId(grpcApiAdjustUserBalance.getTransactionId())
+                .build();
+        LOG.info("ApiAdjustUserBalance applied, msg: {}", apiAdjustUserBalance);
+    }
+
 }
