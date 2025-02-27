ca5fd0d51bee39952315f66cf6ccc710d2e8f82a
ming <ming@binance.com>
2025-02-27 14:25:07 +0800

    grpc port可配置

--- raft-exchange-server/src/main/java/com/binance/raftexchange/server/grpc/ApiService.java
+++ raft-exchange-server/src/main/java/com/binance/raftexchange/server/grpc/ApiService.java
@@ -1,40 +1,38 @@
 package com.binance.raftexchange.server.grpc;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.binance.raftexchange.stubs.api.ApiCommand;
 import com.binance.raftexchange.stubs.api.ApiCommandServiceGrpc;
 import com.binance.raftexchange.stubs.command.CommandResult;
 import com.binance.raftexchange.stubs.command.CommandResultCode;
+
 import io.grpc.stub.StreamObserver;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Component;
 
-@Component
 public class ApiService extends ApiCommandServiceGrpc.ApiCommandServiceImplBase {
-    static final Logger LOGGER = LoggerFactory.getLogger(ApiService.class);
+	static final Logger LOGGER = LoggerFactory.getLogger(ApiService.class);
 
-    @Override
-    public StreamObserver<ApiCommand> execApiCommand(StreamObserver<CommandResult> responseObserver) {
-        return new StreamObserver<ApiCommand>() {
-            @Override
-            public void onNext(ApiCommand apiCommand) {
-                CommandResult result = CommandResult.newBuilder()
-                        .setResultCode(CommandResultCode.ACCEPTED)
-                        .build();
-                responseObserver.onNext(result);
-                responseObserver.onCompleted();
-            }
+	@Override
+	public StreamObserver<ApiCommand> execApiCommand(StreamObserver<CommandResult> responseObserver) {
+		return new StreamObserver<ApiCommand>() {
+			@Override
+			public void onNext(ApiCommand apiCommand) {
+				CommandResult result = CommandResult.newBuilder().setResultCode(CommandResultCode.ACCEPTED).build();
+				responseObserver.onNext(result);
+				responseObserver.onCompleted();
+			}
 
-            @Override
-            public void onError(Throwable throwable) {
-                //先log下。。。
-                LOGGER.error("error ", throwable);
-            }
+			@Override
+			public void onError(Throwable throwable) {
+				// 先log下。。。
+				LOGGER.error("error ", throwable);
+			}
 
-            @Override
-            public void onCompleted() {
+			@Override
+			public void onCompleted() {
 
-            }
-        };
-    }
+			}
+		};
+	}
 }
