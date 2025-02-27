2346a9d7d39d2648b085496c90abb7639579aa97
ming <ming@binance.com>
2025-02-27 14:28:45 +0800

    grpc port可配置

--- raft-exchange-server/src/main/java/com/binance/raftexchange/server/RaftExchangeApplication.java
+++ raft-exchange-server/src/main/java/com/binance/raftexchange/server/RaftExchangeApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+import com.binance.platform.common.shutdown.GracefulShutdownHook;
 import com.binance.raftexchange.server.grpc.GrpcServerContainer;
 import com.binance.raftexchange.server.raft.RaftClusterContainer;
 import com.binance.raftexchange.server.raft.RaftClusterDiscovery;
@@ -16,7 +17,7 @@ import com.netflix.discovery.EurekaClient;
 
 @EnableEurekaClient
 @SpringBootApplication
-public class RaftExchangeApplication implements CommandLineRunner {
+public class RaftExchangeApplication implements CommandLineRunner, GracefulShutdownHook {
 	private static final Logger LOGGER = LoggerFactory.getLogger(RaftExchangeApplication.class);
 
 	public static void main(String[] args) throws Exception {
@@ -30,6 +31,8 @@ public class RaftExchangeApplication implements CommandLineRunner {
 	private ApplicationInfoManager applicationInfoManager;
 
 	private RaftClusterContainer raftClusterContainer;
+
+	private GrpcServerContainer grpcServerContainer;
 
 	@Override
 	public void run(String... arg0) throws Exception {
@@ -47,6 +50,25 @@ public class RaftExchangeApplication implements CommandLineRunner {
 	public void startGrpcServer() throws Exception {
 		GrpcServerContainer grpcServerContainer = new GrpcServerContainer();
 		grpcServerContainer.doStart();
+		this.grpcServerContainer = grpcServerContainer;
+	}
+
+	@Override
+	public void shutdown() {
+		if (this.raftClusterContainer != null) {
+			try {
+				this.raftClusterContainer.doStop();
+			} catch (Exception e) {
+				LOGGER.error(e.getMessage(), e);
+			}
+		}
+		if (this.grpcServerContainer != null) {
+			try {
+				this.grpcServerContainer.doStop();
+			} catch (Exception e) {
+				LOGGER.error(e.getMessage(), e);
+			}
+		}
 	}
 
 }
