2812a548d1a3974789b3fe807296b516d1b02340
ming <ming@binance.com>
2025-03-05 10:11:49 +0800

    update

--- raft-exchange-server/src/main/java/com/binance/raftexchange/server/grpc/UniversalInterceptor.java
+++ raft-exchange-server/src/main/java/com/binance/raftexchange/server/grpc/UniversalInterceptor.java
@@ -7,11 +7,6 @@ import java.util.concurrent.CancellationException;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ConcurrentHashMap;
 
-import com.binance.raftexchange.stubs.request.ApiCommand;
-import io.grpc.ForwardingServerCallListener;
-import io.grpc.Metadata;
-import io.grpc.ServerCall;
-import io.grpc.Status;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -19,19 +14,26 @@ import com.binance.raftexchange.server.raft.RaftClusterContainer;
 import com.binance.raftexchange.server.raft.RaftNode;
 import com.binance.raftexchange.server.util.SerializeHelper;
 import com.binance.raftexchange.server.util.ThrowableFunction;
+import com.binance.raftexchange.stubs.request.ApiCommand;
 import com.binance.raftexchange.stubs.response.CommandResult;
 import com.binance.raftexchange.stubs.response.CommandResultCode;
 import com.binance.raftexchange.stubs.response.ServerNode;
 
+import io.grpc.ForwardingServerCallListener;
+import io.grpc.Metadata;
+import io.grpc.ServerCall;
+import io.grpc.Status;
+
 class UniversalInterceptor<ReqT, RespT> extends ForwardingServerCallListener.SimpleForwardingServerCallListener<ReqT> {
     private static final Logger LOGGER = LoggerFactory.getLogger(UniversalInterceptor.class);
-
     protected final ServerCall<ReqT, RespT> call;
     protected final RaftClusterContainer raftClusterContainer;
 
-    protected final ConcurrentHashMap<ReqT, CompletableFuture<CommandResult>> commandOnTheWay = new ConcurrentHashMap<>();
+    protected final ConcurrentHashMap<ReqT, CompletableFuture<CommandResult>> commandOnTheWay =
+        new ConcurrentHashMap<>();
 
-    public UniversalInterceptor(RaftClusterContainer raftClusterContainer, ServerCall.Listener<ReqT> delegate, ServerCall<ReqT, RespT> call) {
+    public UniversalInterceptor(RaftClusterContainer raftClusterContainer, ServerCall.Listener<ReqT> delegate,
+        ServerCall<ReqT, RespT> call) {
         super(delegate);
         this.raftClusterContainer = raftClusterContainer;
         this.call = call;
@@ -39,24 +41,27 @@ class UniversalInterceptor<ReqT, RespT> extends ForwardingServerCallListener.Sim
 
     @Override
     public void onMessage(ReqT message) {
-        try (InputStream stream = (InputStream) message) {
+        try (InputStream stream = (InputStream)message) {
             stream.markSupported();
-            //这里的InputStream是一个对于netty direct的bytebuf的安全包装
-            //基本可以认为是零拷贝的
-            CompletableFuture<CommandResult> complete = handle(readAll(stream))
-                    .whenComplete((result, err) -> {
-                        commandOnTheWay.remove(message);
-                        call.sendHeaders(new Metadata());
-                        if (result != null) {
-                            call.sendMessage((RespT) new ByteArrayInputStream(result.toByteArray()));
-                            return;
-                        }
-                        if (err instanceof CancellationException) {
-                            return;
-                        }
-                        LOGGER.error("exchange core error!", err);
-                        call.close(Status.INTERNAL.withCause(err), new Metadata());
-                    });
+            /**
+             * @formatter off
+             */
+            CompletableFuture<CommandResult> complete = handle(readAll(stream)).whenComplete((result, err) -> {
+                commandOnTheWay.remove(message);
+                call.sendHeaders(new Metadata());
+                if (result != null) {
+                    call.sendMessage((RespT)new ByteArrayInputStream(result.toByteArray()));
+                    return;
+                }
+                if (err instanceof CancellationException) {
+                    return;
+                }
+                LOGGER.error("exchange core error!", err);
+                call.close(Status.INTERNAL.withCause(err), new Metadata());
+            });
+            /**
+             * @formatter on
+             */
             commandOnTheWay.put(message, complete);
         } catch (Exception e) {
             throw new RuntimeException(e);
@@ -74,7 +79,6 @@ class UniversalInterceptor<ReqT, RespT> extends ForwardingServerCallListener.Sim
     public void onComplete() {
         LOGGER.info("client onComplete");
         for (CompletableFuture<CommandResult> future : commandOnTheWay.values()) {
-            //对端关闭了 取消后续任务
             future.cancel(false);
         }
         delegate().onComplete();
@@ -84,18 +88,14 @@ class UniversalInterceptor<ReqT, RespT> extends ForwardingServerCallListener.Sim
         if (!raftClusterContainer.isLeader()) {
             RaftNode raftNode = raftClusterContainer.leaderNode();
             ServerNode leaderNode = Transformer.raftNodeTransform(raftNode);
-            return CompletableFuture.completedFuture(
-                    CommandResult.newBuilder()
-                            .setResultCode(CommandResultCode.NEED_MOVE)
-                            .setLeaderNode(leaderNode)
-                            .build()
-            );
+            return CompletableFuture.completedFuture(CommandResult.newBuilder()
+                .setResultCode(CommandResultCode.NEED_MOVE).setLeaderNode(leaderNode).build());
         }
 
         byte[] raftLog = SerializeHelper.serializeWithType(ApiCommand.class, apiCommand);
         return raftClusterContainer.requestConsensus(raftLog)
-                .thenApply(ThrowableFunction.warp(b -> SerializeHelper.bytesToEnumProto(b, CommandResultCode.class))) // 把状态机应用结果给下面
-                .thenApply(v -> CommandResult.newBuilder().setResultCode(v).build());
+            .thenApply(ThrowableFunction.warp(b -> SerializeHelper.bytesToEnumProto(b, CommandResultCode.class))) // 把状态机应用结果给下面
+            .thenApply(v -> CommandResult.newBuilder().setResultCode(v).build());
     }
 
     protected boolean allowFollowExecute(byte[] command) {
