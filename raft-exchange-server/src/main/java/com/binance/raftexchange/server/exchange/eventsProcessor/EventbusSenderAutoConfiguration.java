eb8126aaca4a3fe1437c8530b54d1f9ff2ddbf03
ocean.s <ocean.s@binance.com>
2025-03-06 22:39:25 +0800

    1,支持kafka

--- raft-exchange-server/src/main/java/com/binance/raftexchange/server/exchange/eventsProcessor/EventbusSenderAutoConfiguration.java
+++ raft-exchange-server/src/main/java/com/binance/raftexchange/server/exchange/eventsProcessor/EventbusSenderAutoConfiguration.java
@@ -1,19 +1,39 @@
 package com.binance.raftexchange.server.exchange.eventsProcessor;
 
-import com.binance.platform.eventbus.zeromq.ZMQEventBusListener;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.ConfigurableEnvironment;
+
+import java.util.Properties;
 
 @Configuration
-public class EventbusSenderAutoConfiguration implements ApplicationListener<ApplicationReadyEvent> {
+public class EventbusSenderAutoConfiguration implements InitializingBean {
+
+    @Value("${raftexchange.kafka.boostrap.servers}")
+    public String servers;
+
+    @Value("${raftexchange.eventbus.topic}")
+    public String topic;
+
+    public KafkaProducer<Long, byte[]> produce() {
+        //手动搞下 避免spring kafka引入的复杂度
+        Properties properties = new Properties();
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, servers);
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.LongSerializer");
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.ByteArraySerializer");
+        properties.setProperty(ProducerConfig.ACKS_CONFIG, "all");
+        properties.setProperty(ProducerConfig.PARTITIONER_IGNORE_KEYS_CONFIG, "false");
+        properties.setProperty(ProducerConfig.PARTITIONER_CLASS_CONFIG, KafkaSender.CommandPartitioner.class.getName());
+        return new KafkaProducer<Long, byte[]>(properties);
+    }
+
     @Override
-    public void onApplicationEvent(ApplicationReadyEvent event) {
-        ConfigurableEnvironment environment = event.getApplicationContext().getEnvironment();
-        String topic = environment.getProperty("raftexchange.eventbus.topic");
-        String appName = environment.getProperty("spring.application.name");
-        ZMQEventBusListener listener = event.getApplicationContext().getBean(ZMQEventBusListener.class);
-        EventBusSender.SENDER = new EventBusSender(listener, topic, appName);
+    public void afterPropertiesSet() throws Exception {
+        KafkaProducer<Long, byte[]> produce = produce();
+        KafkaSender.INSTANCE = new KafkaSender(produce, topic);
     }
 }
