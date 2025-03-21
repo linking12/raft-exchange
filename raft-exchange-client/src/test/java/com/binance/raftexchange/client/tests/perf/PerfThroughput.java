5072894a5fc7b3cf4bf0008dac911599c88c6cb5
bob.dong <bob.dong@binance.com>
2025-03-21 14:20:56 +0400

    e2e test

---
+++ raft-exchange-client/src/test/java/com/binance/raftexchange/client/tests/perf/PerfThroughput.java
@@ -0,0 +1,23 @@
+package com.binance.raftexchange.client.tests.perf;
+
+import com.binance.raftexchange.client.tests.util.TestDataParameters;
+import com.binance.raftexchange.client.tests.util.ThroughputTestsModule;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+/**
+ * 这里都是client端，记得参考core的PerformanceConfiguration 启动raft-exchange服务端
+ * 我们的服务端没有CLEAN_TEST，可以删除snapshot实现
+ * 我们的服务端一定会打snapshot，可以调长snapshot的时间实现
+ */
+@Slf4j
+final class PerfThroughput {
+
+    @Test
+    void testThroughputMargin() {
+        ThroughputTestsModule.throughputTestImpl(
+                TestDataParameters.singlePairMarginBuilder().build(),
+                50);
+    }
+
+}
---
+++ raft-exchange-client/src/test/java/com/binance/raftexchange/client/tests/util/TestDataParameters.java
@@ -0,0 +1,32 @@
+package com.binance.raftexchange.client.tests.util;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.Set;
+
+@Data
+@Builder
+public class TestDataParameters {
+    public final int totalTransactionsNumber;
+    public final int targetOrderBookOrdersTotal;
+    public final int numAccounts;
+    public final Set<Integer> currenciesAllowed;
+    public final int numSymbols;
+    public final ExchangeTestContainer.AllowedSymbolTypes allowedSymbolTypes;
+    public final TestOrdersGeneratorConfig.PreFillMode preFillMode;
+    public final boolean avalancheIOC;
+
+    public static TestDataParametersBuilder singlePairMarginBuilder() {
+        return TestDataParameters.builder()
+                .totalTransactionsNumber(300_000) // 3w 300w 不影响tps
+                .targetOrderBookOrdersTotal(1000)
+                .numAccounts(2000)
+                .currenciesAllowed(TestConstants.CURRENCIES_FUTURES)
+                .numSymbols(1)
+                .allowedSymbolTypes(ExchangeTestContainer.AllowedSymbolTypes.FUTURES_CONTRACT)
+                .preFillMode(TestOrdersGeneratorConfig.PreFillMode.ORDERS_NUMBER);
+    }
+
+
+}
