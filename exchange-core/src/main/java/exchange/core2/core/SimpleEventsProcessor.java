363bd259f0a24f31d2144661dc2621a3574a50e4
ming <ming@binance.com>
2025-09-11 11:24:35 +0800

    update

--- exchange-core/src/main/java/exchange/core2/core/SimpleEventsProcessor.java
+++ exchange-core/src/main/java/exchange/core2/core/SimpleEventsProcessor.java
@@ -30,7 +30,7 @@ public class SimpleEventsProcessor implements ObjLongConsumer<OrderCommand> {
 
     private final ITradeEventsHandler tradeEventsHandler;
     private final IFundEventsHandler fundEventsHandler;
-    private final IntObjectHashMap<UserProfileService> userProfileServices = new IntObjectHashMap<UserProfileService>();
+    private final IntObjectHashMap<UserProfileService> userProfileServices = IntObjectHashMap.newMap();
     @Setter
     private SymbolSpecificationProvider symbolSpecificationProvider;
     @Setter
