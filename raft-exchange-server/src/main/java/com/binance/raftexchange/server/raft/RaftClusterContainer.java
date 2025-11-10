49f6d2ee2effd33b47e3ff8af75c98680ec35567
ocean.s <ocean.s@binance.com>
2025-11-10 14:10:59 +0800

    使用租约读进行优化读性能

--- raft-exchange-server/src/main/java/com/binance/raftexchange/server/raft/RaftClusterContainer.java
+++ raft-exchange-server/src/main/java/com/binance/raftexchange/server/raft/RaftClusterContainer.java
@@ -66,7 +66,7 @@ public class RaftClusterContainer {
         RaftOptions raftOptions = new RaftOptions();
         raftOptions.setDisruptorBufferSize(8 * 1024 * 1024); // 撮合5MT/s，buffer给到8M条，每条log(用户数据[ApiCommand]+元数据[idx term
                                                              // type])大概是128字节，大约1G内存
-        raftOptions.setReadOnlyOptions(ReadOnlyOption.ReadOnlySafe); // 先使用最保守的readIndex优化
+        raftOptions.setReadOnlyOptions(ReadOnlyOption.ReadOnlyLeaseBased);
         nodeOptions.setRaftOptions(raftOptions);
         nodeOptions.setInitialConf(conf);
         int snapshotIntervalSecs = Integer.parseInt(System.getProperty("raft-exchange.snapshot.interval", "28800")); // 8h
